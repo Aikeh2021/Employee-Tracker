03d82473e5f04d18ecb0ad4863071bc8ca15215d
Ashley Ikeh <ashley.ikeh25@gmail.com>
2021-01-25 12:35:13 -0500

    Added some data to the seeds.sql file.

--- seeds.sql
+++ seeds.sql
@@ -49,3 +49,27 @@ CREATE TABLE employee (
   SELECT * FROM department;
   SELECT * FROM role;
   SELECT * FROM employee;
+
+
+-- Creates new rows containing data in the named columns --
+INSERT INTO department (name)
+VALUES ("Sales"), ("Engineering"), ("Finance"), ("Legal");
+
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Software Engineer", 120000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Sales Lead", 100000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Salesperson", 80000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Lead Engineer", 150000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Accountant", 125000, 3);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Legal Team Lead", 250000, 4);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Lawyer", 190000, 4);
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Ashley", "Ikeh", 4);
