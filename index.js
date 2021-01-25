0750628126d17d41e9c0537bf799eb224506c1bb
Ashley Ikeh <ashley.ikeh25@gmail.com>
2021-01-25 09:42:50 -0500

    Started writing out conditionals for each of the possible outcomes for question1.

--- index.js
+++ index.js
@@ -11,5 +11,25 @@ inquirer.prompt([
     }
 ])
 .then((response) => {
-    console.log(response);
+    if(response.question1 === "View all employess"){
+        console.log("Read employee table");
+    }
+    else if(response.question1 === "View all employees by department"){
+        console.log("Read employees by department");
+    }
+    else if(response.question1 === "View all employees by manager"){
+        console.log("Read employees by manager");
+    }
+    else if(response.question1 === "Add employee"){
+        console.log("Insert into employee table");
+    }
+    else if(response.question1 === "Remove employee"){
+        console.log("Delete from employee table");
+    }
+    else if(response.question1 === "Update employee role"){
+        console.log("Update employee with id of blank, role to blank");
+    }
+    else if(response.question1 === "Update employee manager"){
+        console.log("Update employee with id of blank, manager to blank");
+    }
 });
